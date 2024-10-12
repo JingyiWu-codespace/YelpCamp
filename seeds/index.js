dbd35983c9d104bce5df5a3c50652a12e4df7f53
Jingyi (Jennie) Wu <140898578+JingyiWu-codespace@users.noreply.github.com>
2024-10-13 01:29:42 +0200

    Add basic css

--- seeds/index.js
+++ seeds/index.js
@@ -17,11 +17,15 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
-
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10
     const camp = new Campground({
+
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus rerum odit doloremque nemo fugit tempore molestiae consectetur harum! ',
+      price
     })
     await camp.save()
   }
