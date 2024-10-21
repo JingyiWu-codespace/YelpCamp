edc20b3cbda9e912f370f358edc3332b51cd74f6
Jingyi (Jennie) Wu <140898578+JingyiWu-codespace@users.noreply.github.com>
2024-10-22 00:57:27 +0200

    add Cloudinary

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,15 +2,20 @@ const express = require('express');
 const router = express.Router();
 const campgrounds = require('../controllers/campgrounds.js')
 const catchAsync = require('../utils/catchAsync');
-
 const { isLoggedIn, isAuth, validateCampground } = require('../middleware.js');
-
+const multer = require('multer')
 const ExpressError = require('../utils/ExpressError')
+const { storage } = require('../cloudinary')
+const upload = multer({ storage })
 const Campground = require('../models/campground');
 
 router.route('/')
   .get(catchAsync(campgrounds.index))
-  .post(isLoggedIn, validateCampground, catchAsync(campgrounds.creatCampground))
+  //.post(isLoggedIn, validateCampground, catchAsync(campgrounds.creatCampground))
+  .post(upload.single('image'), (req, res) => {
+    res.status(200).send({ body: req.body, file: req.file });
+
+  })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
